4a5ae708a3ee6c68081fc40ab794fd59d6d1da30
Jayant Babu <jayantbabu@microsoft.com>
2024-06-01 18:17:41 +0530

    Setting up the sql database connection

--- Hibernate and JPA CRUD learning/HibernateJPACrud/src/main/java/com/jayantsharma/HibernateJPACrud/HibernateJpaCrudApplication.java
+++ Hibernate and JPA CRUD learning/HibernateJPACrud/src/main/java/com/jayantsharma/HibernateJPACrud/HibernateJpaCrudApplication.java
@@ -1,13 +1,25 @@
 package com.jayantsharma.HibernateJPACrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HibernateJpaCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateJpaCrudApplication.class, args);
+	}
+	// CommandLineRunner is from the springboot interface
+	// This tool is provided by the Spring framework for quickly developing and testing Spring Boot applications from the command prompt.
+	// The code is executed after the Spring Beans have loaded.
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args)
+	{
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
