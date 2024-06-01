94d5946fa1adfb0cde70c482676ebc98c97a7b1b
Jayant Babu <jayantbabu@microsoft.com>
2024-06-01 15:34:52 +0530

    First commit for the hiberanate learning

---
+++ Hibernate and JPA CRUD learning/HibernateJPACrud/src/main/java/com/jayantsharma/HibernateJPACrud/HibernateJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jayantsharma.HibernateJPACrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateJpaCrudApplication.class, args);
+	}
+
+}
