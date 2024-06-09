61baada11c57f91c63c5596380bea455c57d9711
Jayant Babu <jayantbabu@microsoft.com>
2024-06-09 16:49:59 +0530

    First commit for REST controller learning

---
+++ REST Learning CRUD plus security/RESTCRUDLearning/src/main/java/com/jayantsharma/RESTCRUDLearning/RestcrudLearningApplication.java
@@ -0,0 +1,13 @@
+package com.jayantsharma.RESTCRUDLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcrudLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcrudLearningApplication.class, args);
+	}
+
+}
---
+++ REST Learning CRUD plus security/RESTCRUDLearning/src/test/java/com/jayantsharma/RESTCRUDLearning/RestcrudLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayantsharma.RESTCRUDLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcrudLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
